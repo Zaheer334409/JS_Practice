8f5e6fff0c22d513a80da64213cf907609370a49
Zaheer Ali <zaheerali334409@gmail.com>
2025-09-19 16:47:27 +0500

    updated

---
+++ DOM/app.js
@@ -0,0 +1,43 @@
+// console.log("Hello World");
+
+// Create Element in Javascript
+
+
+// let firstName = document.getElementById("firstName");
+// let secondName = document.getElementById("secondName");
+
+// secondName.innerHTML = "Programmer Life is Very Happay!😂"
+
+
+// ToDo_List
+// let input = document.getElementById("input");
+// let para = document.getElementById("para");
+// let ul = document.getElementById("ul");
+// let item;
+
+// function result() {
+//     if (input == "") {
+//         para.innerHTML = "Empty Text";
+//         para.style.color = "red";
+//     } else {
+//         let li = document.createElement("li");
+//         let liText = document.createTextNode(input.value);
+//         li.appendChild(liText);
+
+//         let deleteBtn = document.createElement("button");
+//         let deleteBTnText = document.createTextNode("Delete Btn");
+//         deleteBtn.appendChild(deleteBTnText);
+
+//         let editBtn = document.createElement("button");
+//         let editBTnText = document.createTextNode("Edit Btn");
+//         editBtn.appendChild(editBTnText);
+
+//         ul.appendChild(li);
+//         li.appendChild(deleteBtn);
+//         li.appendChild(editBtn);
+//         input.value = ""
+
+//     }
+// }
+// Color_Changer
+
